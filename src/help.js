b9033ef5005418841e31f8e94225b90c5f5ce483
Cyber Walrus <stonewalrusfan@gmail.com>
2018-10-12 18:19:25 +0300

    описание зеленым цветом

--- src/help.js
+++ src/help.js
@@ -16,10 +16,10 @@ module.exports = {
     return command === HELP_COMMAND;
   },
   execute() {
-    let message = `Доступные команды:\n${HELP_COMMAND.italic.gray} — печатает этот текст;`;
+    let message = `Доступные команды:\n${HELP_COMMAND.italic.gray} — ${`печатает этот текст`.green}`;
     commands.forEach(function (item) {
       if (item.name) {
-        message += `\n${item.name.italic.gray} — ${item.description};`;
+        message += `\n${item.name.italic.gray} — ${item.description.green}`;
       }
     });
     console.log(message);

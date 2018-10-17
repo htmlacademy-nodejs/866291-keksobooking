ee7fe318f7d0f1dc051161d6a6f9c14829981054
Pakhomov Andrey <stonewalrusfan@gmail.com>
2018-10-17 12:37:52 +0300

    изменение тестов

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -6,13 +6,16 @@ const {generateEntity} = require(`../generator/generator-keksobooking`);
 const GENERATE_COMMAND = `--generate`;
 const DEFAULT_PATH = `${process.cwd()}/keksobooking.json`;
 
-const data = generateEntity();
+const data = [];
 const fileWriteOptions = {encoding: `utf-8`, mode: 0o644};
 
 module.exports = {
   name: GENERATE_COMMAND,
   description: `генерирует данные приложения`,
-  execute(command, path = DEFAULT_PATH) {
+  execute(command, quantity = 1, path = DEFAULT_PATH) {
+    for (let i = 0; i < quantity; i++) {
+      data.push(generateEntity());
+    }
     return new Promise((success, fail) => {
       fs.writeFile(path, JSON.stringify(data), fileWriteOptions, (err) => {
         if (err) {
--- test/generate.test.js
+++ test/generate.test.js
@@ -7,12 +7,14 @@ const generateCommand = require(`../src/commands/generate`);
 
 const access = promisify(fs.access);
 const unlink = promisify(fs.unlink);
+const COMMAND = ``;
+const QUANTITY_OBJECT = 10;
 
 describe(`Generate JSON command`, function () {
   it(`should fail on non existing folder`, function () {
-    const tempFileName = `${__dirname}/folder/testfile.json`;
+    const tempFileName = `${__dirname}/json/testfile.json`;
 
-    return generateCommand.execute(tempFileName)
+    return generateCommand.execute(COMMAND, QUANTITY_OBJECT, tempFileName)
       .then(() => assert.fail(`Path ${tempFileName} should not be available`))
       .catch((e) => assert.ok(e));
   });
@@ -20,8 +22,16 @@ describe(`Generate JSON command`, function () {
   it(`should create new file`, function () {
     const tempFileName = `${__dirname}/testfile.json`;
 
-    return generateCommand.execute(tempFileName)
+    return generateCommand.execute(COMMAND, QUANTITY_OBJECT, tempFileName)
       .then(access(tempFileName))
       .then(unlink(tempFileName));
+  });
+  it(`check JSON file`, function () {
+    const tempFileName = `${__dirname}/json/testfile.json`;
+
+    let testJSON = require(tempFileName);
+    for (let item of testJSON) {
+      console.log(item);
+    }
   });
 });

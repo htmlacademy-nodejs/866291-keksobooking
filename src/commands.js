84d6cdf3a90f46167c2367d48e653a023a9446ff
Cyber Walrus <stonewalrusfan@gmail.com>
2018-10-23 06:31:42 +0300

    консоль

--- src/commands.js
+++ src/commands.js
@@ -1,7 +1,15 @@
 'use strict';
 
 require(`colors`);
+const fs = require(`fs`);
 const packageInfo = require(`../package`);
+const readline = require(`readline`);
+const path = require(`path`);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 const commands = [
   require(`./commands/help`),
   require(`./commands/version`),
@@ -12,17 +20,72 @@ const commands = [
   require(`./commands/default`),
 ];
 const DEFAULT_COMMAND = `default`;
+const GENERATE_COMMAND = `--generate`;
+const DEFAULT_PATH = path.resolve(`keksobooking.json`);
 
 let isApplicable = function (item, command) {
   return item.name === command || item.name === DEFAULT_COMMAND;
 };
 
+let quantityNew = 1;
+let pathFileNew = DEFAULT_PATH;
+const fileExists = (filePath) => {
+  try {
+    return fs.statSync(filePath).isFile();
+  } catch (err) {
+    return false;
+  }
+};
+const enterAccept = (answer) => {
+  if (answer.match(/^y(es)?$/i)) {
+    rl.question(`Сколько элементов нужно создать? (введите целое число): `, enterNumberConsole);
+  } else if (answer.match(/^e(nd)?$/i) || answer.match(/^n(o)?$/i)) {
+    rl.close();
+  } else {
+    rl.question(`Cгенерировать данные? (yes/no) : `, enterAccept);
+  }
+};
+const enterNumberConsole = (answer) => {
+  if (answer >= 0) {
+    quantityNew = answer;
+    rl.question(`Введите путь к файлу : `, enterPathFile);
+  } else if (answer.match(/^e(nd)?$/i)) {
+    rl.close();
+  } else {
+    rl.question(`Введите число правильно или закройте програму командой end : `, enterNumberConsole);
+  }
+};
+const enterPathFile = (answer) => {
+  if (fileExists(answer)) {
+    pathFileNew = answer;
+    rl.question(`Перезаписать файл? (yes/no) : `, enterAcceptFile);
+  } else if (answer) {
+    pathFileNew = answer;
+    commands.find((item) => isApplicable(item, GENERATE_COMMAND)).execute(GENERATE_COMMAND, quantityNew, pathFileNew);
+    rl.close();
+  } else if (answer.match(/^e(nd)?$/i)) {
+    rl.close();
+  } else {
+    rl.question(`Введите путь правильно или закройте програму командой end : `, enterPathFile);
+  }
+};
+const enterAcceptFile = (answer) => {
+  if (answer.match(/^y(es)?$/i)) {
+    commands.find((item) => isApplicable(item, GENERATE_COMMAND)).execute(GENERATE_COMMAND, quantityNew, pathFileNew);
+    rl.close();
+  } else if (answer.match(/^e(nd)?$/i) || answer.match(/^n(o)?$/i)) {
+    rl.close();
+  } else {
+    rl.question(`Введите число правильно или закройте програму командой end : `, enterNumberConsole);
+  }
+};
 module.exports = {
   check(command) {
     if (!command) {
       console.log(`Привет пользователь!\nЭта программа будет запускать сервер «${packageInfo.name.green}».\nАвтор: ${packageInfo.author.blue}.`);
-      process.exit(1);
+      rl.question(`Cгенерировать данные? (yes/no) : `, enterAccept);
+    } else {
+      commands.find((item) => isApplicable(item, command)).execute(command);
     }
-    commands.find((item) => isApplicable(item, command)).execute(command);
   }
 };
--- src/commands/generate.js
+++ src/commands/generate.js
@@ -8,55 +8,19 @@ const {generateEntity} = require(`../generator/generator-keksobooking`);
 const GENERATE_COMMAND = `--generate`;
 const DEFAULT_PATH = path.resolve(`keksobooking.json`);
 const writeFile = promisify(fs.writeFile);
-const readline = require(`readline`);
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 const data = [];
 const fileWriteOptions = {encoding: `utf-8`, mode: 0o644};
-let quantityNew = 1;
-let pathFileNew = DEFAULT_PATH;
-const enterNumberConsole = (answer) => {
-  if (answer >= 0) {
-    quantityNew = answer;
-    rl.question(`Введите путь к файлу : `, enterPathFile);
-  } else if (answer.match(/^e(nd)?$/i)) {
-    rl.close();
-  } else {
-    rl.question(`Введите число правильно или закройте програму командой end : `, enterNumberConsole);
-  }
-};
-const enterPathFile = (answer) => {
-  if (answer) {
-    executeFun(quantityNew, pathFileNew);
-  } else if (answer.match(/^e(nd)?$/i)) {
-    rl.close();
-  } else {
-    rl.question(`Введите путь правильно или закройте програму командой end : `, enterPathFile);
-  }
-};
-const executeFun = (quantity, pathFile) => {
-  rl.close();
-  for (let i = 0; i < quantity; i++) {
-    data.push(generateEntity());
-  }
-  return writeFile(pathFile, JSON.stringify(data), fileWriteOptions)
-    .then(() => console.log(`Файл создан успешно! (${pathFile})`))
-    .catch((error) => console.log(error));
-
-};
 
 module.exports = {
   name: GENERATE_COMMAND,
   description: `генерирует данные приложения`,
-  execute(command, quantity = 1, pathFile = DEFAULT_PATH, isTest = false) {
-    if (!isTest) {
-      return rl.question(`Сколько элементов нужно создать? (введите целое число): `, enterNumberConsole);
-    } else {
-      return executeFun(quantity, pathFile);
+  execute(command, quantity = 1, pathFile = DEFAULT_PATH) {
+    for (let i = 0; i < quantity; i++) {
+      data.push(generateEntity());
     }
+    return writeFile(pathFile, JSON.stringify(data), fileWriteOptions)
+      .then(() => console.log(`Файл создан успешно! (${pathFile})`))
+      .catch((error) => console.log(error));
   }
 };

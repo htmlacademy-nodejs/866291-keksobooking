66312dbbd4d60fe1269c2bf80fdebd94ce44f97b
Cyber Walrus <stonewalrusfan@gmail.com>
2018-10-26 02:38:15 +0300

    сервер и тест

--- src/commands/server.js
+++ src/commands/server.js
@@ -1,45 +1,14 @@
 'use strict';
 
-const http = require(`http`);
-const url = require(`url`);
-const fs = require(`fs`);
-const {promisify} = require(`util`);
+const express = require(`express`);
 const path = require(`path`);
+const app = express();
 
+const SERVER_COMMAND = `--server`;
 const MAX_PORT = 49151;
 const MIN_PORT = 1024;
-const HOSTNAME = `127.0.0.1`;
 const PORT = 3000;
-const SERVER_COMMAND = `--server`;
-const CONTENT_TYPE = {
-  'css': `text/css`,
-  'html': `text/html; charset=UTF-8`,
-  'jpg': `image/jpeg`,
-  'png': `image/png`,
-  'gif': `image/gif`,
-  'svg': `image/svg+xml`,
-  'ico': `image/x-icon`
-};
 
-const stat = promisify(fs.stat);
-const readdir = promisify(fs.readdir);
-const readfile = promisify(fs.readFile);
-
-const printDirectory = (pathURL, relativePath, files) => {
-  relativePath = relativePath === `/` ? `` : relativePath;
-  return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Static</title>
-</head>
-<body>
-<ul>
-    ${files.map((it) => `<li><a href="${relativePath}/${it}">${it}</a></li>`).join(``)}
-</ul>
-</body>
-</html>`;
-};
 const checkPort = (port) => {
   if (port >= MIN_PORT && port <= MAX_PORT) {
     return port;
@@ -48,61 +17,46 @@ const checkPort = (port) => {
   }
   return PORT;
 };
-const readFile = async (pathFile, res) => {
-  const data = await readfile(pathFile);
-  const content = CONTENT_TYPE[path.extname(pathFile).replace(`.`, ``)];
-  const contentType = content ? content : `text/plain`;
-  res.setHeader(`content-type`, contentType);
-  res.end(data);
+
+const BAD_REQUEST = (req, res) => {
+  res.status(400).send(`Bad request`);
 };
 
-
-const readDir = async (pathDir, relativePath, res) => {
-  const files = await readdir(pathDir);
-  res.setHeader(`content-type`, `text/html`);
-  res.end(printDirectory(pathDir, relativePath, files));
+const NOT_FOUND_HANDLER = (req, res) => {
+  res.status(404).send(`Page was not found`);
 };
 
-const server = http.createServer((req, res) => {
+const ERROR_HANDLER = (err, req, res, _next) => {
+  if (err) {
+    console.error(err);
+    res.status(err.code || 500).send(err.message);
+  }
+};
 
-  const localPath = url.parse(req.url).pathname;
-  const absolutePath = path.resolve(`static`, `${localPath.replace(`/`, ``)}`);
+app.use(express.static(path.resolve(`static`)));
 
-  (async () => {
-    try {
-      const pathStat = await stat(absolutePath);
+app.use(BAD_REQUEST);
 
-      res.statusCode = 200;
-      res.statusMessage = `OK`;
+app.use(NOT_FOUND_HANDLER);
 
-      if (pathStat.isDirectory()) {
-        await readDir(absolutePath, localPath, res);
-      } else {
-        await readFile(absolutePath, res);
-      }
-    } catch (e) {
-      res.writeHead(404, `Not Found`);
-      res.end();
-    }
-  })().catch((e) => {
-    res.writeHead(500, e.message, {
-      'content-type': `text/plain`
-    });
-    res.end(e.message);
-  });
-});
+app.use(ERROR_HANDLER);
+
+const runServer = (port) => {
+
+  port = parseInt(port, 10);
+
+  app.listen(port, () => console.log(`Сервер запущен: http://localhost:${port}`));
+};
 
 module.exports = {
   name: SERVER_COMMAND,
   description: `запускает сервер`,
   execute(command, port = PORT) {
     port = checkPort(port);
-    server.listen(port, HOSTNAME, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`Сервер запущен http://${HOSTNAME}:${port}`);
-    });
+    runServer(port);
   }
 };
+
+if (require.main === module) {
+  runServer(3000);
+}
---
+++ test/get-offer.test.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const request = require(`supertest`);
+const assert = require(`assert`);
+
+const app = require(`../src/commands/server`).app;
+const DATE = 1539755469933;
+const DATE_FAIL = 1;
+
+describe(`GET /api/offer`, () => {
+  it(`get all offer`, async () => {
+
+    const response = await request(app).
+      get(`/api/offer`).
+      set(`Accept`, `application/json`).
+      expect(200).
+      expect(`Content-Type`, /json/);
+
+    const offer = response.body;
+    assert.equal(offer.length, 17);
+  });
+
+  it(`get all offer with / at the end`, async () => {
+
+    const response = await request(app).
+      get(`/api/offer/`).
+      set(`Accept`, `application/json`).
+      expect(200).
+      expect(`Content-Type`, /json/);
+
+    const offer = response.body;
+    assert.equal(offer.length, 17);
+  });
+
+  it(`get data from unknown resource`, async () => {
+    return await request(app).
+      get(`/api/oneone`).
+      set(`Accept`, `application/json`).
+      expect(404).
+      expect(`Page was not found`).
+      expect(`Content-Type`, /html/);
+  });
+
+});
+
+describe(`GET /api/offer/:date`, () => {
+  it(`get date ${DATE}`, async () => {
+    const response = await request(app).
+      get(`/api/offer/${encodeURI(`${DATE}`)}`).
+      set(`Accept`, `application/json`).
+      expect(200).
+      expect(`Content-Type`, /json/);
+
+    const date = response.body;
+    assert.strictEqual(date.date, DATE);
+  });
+
+  xit(`get unknown date format`, async () => {
+    return request(app).
+      get(`/api/offer/${encodeURI(`test123`)}`).
+      set(`Accept`, `application/json`).
+      expect(404).
+      expect(`неверный формат запроса`).
+      expect(`Content-Type`, /html/);
+  });
+
+  xit(`get unknown date with ${DATE_FAIL}`, async () => {
+    return request(app).
+      get(`/api/offer/${encodeURI(`${DATE_FAIL}`)}`).
+      set(`Accept`, `application/json`).
+      expect(404).
+      expect(`обьект с <<${DATE_FAIL}>> не найден`).
+      expect(`Content-Type`, /html/);
+  });
+});

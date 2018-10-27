9ac45fc7b59e2708fe5d4e59aa3be8d4f692f3cd
Pakhomov Andrey <stonewalrusfan@gmail.com>
2018-10-27 15:05:43 +0300

    valid

--- src/data/keksobooking.js
+++ src/data/keksobooking.js
@@ -54,5 +54,49 @@ module.exports = {
     MAX_Y: 900,
     MIN_Y: 150
   },
-  DATE_INTERVAL: -7
+  DATE_INTERVAL: -7,
+  VALID: {
+    FIELDS: [
+      `title`,
+      `type`,
+      `price`,
+      `adress`,
+      `checkin`,
+      `checkout`,
+      `rooms`
+    ],
+    MAX_TITLE: 140,
+    MIN_TITLE: 30,
+    TYPE: [
+      `flat`,
+      `palace`,
+      `house `,
+      `bungalo`,
+    ],
+    MAX_PRICE: 100000,
+    MIN_PRICE: 1,
+    MAX_ADDRESS: 100,
+    MAX_ROOMS: 1000,
+    MIN_ROOMS: 0,
+    FEATURES: [
+      `wifi`,
+      `dishwasher`,
+      `parking`,
+      `washer`,
+      `elevator`,
+      `conditioner`
+    ],
+    JPG: `image/jpg`,
+    PNG: `image/png`,
+    NAME: [
+      `Keks`,
+      `Pavel`,
+      `Nikolay`,
+      `Alex`,
+      `Ulyana`,
+      `Anastasyia`,
+      `Julia`
+    ]
+
+  }
 };
--- src/routes/validate.js
+++ src/routes/validate.js
@@ -1,11 +1,27 @@
 'use strict';
 
 const ValidationError = require(`../error/validation-error`);
+const {VALID} = require(`../data/keksobooking`);
 
 const validate = (data) => {
   const errors = [];
-  if (!data.name) {
-    errors.push(`Field name "name" is required!`);
+  for (let item of VALID.FIELDS) {
+    if (!data[item]) {
+      errors.push(`Нужно поле "${item}"!`);
+    }
+  }
+  if (data.title.length > VALID.MAX_TITLE) {
+    errors.push(`"title" длинее чем ${VALID.MAX_TITLE}`);
+  } else if (data.title.length < VALID.MIN_TITLE) {
+    errors.push(`"title" короче чем ${VALID.MIN_TITLE}`);
+  }
+  if (VALID.TYPE.find(data.type) === undefined) {
+    errors.push(`"type" неправильное значение`);
+  }
+  if (data.price.length > VALID.MAX_TITLE) {
+    errors.push(`"title" длинее чем ${VALID.MAX_TITLE}`);
+  } else if (data.title.length < VALID.MIN_TITLE) {
+    errors.push(`"title" короче чем ${VALID.MIN_TITLE}`);
   }
   if (errors.length > 0) {
     throw new ValidationError(errors);
